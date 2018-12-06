ba1856c3cae481aed55e0db7b963ee5e9d3b8480
MarekOchocki <marcopolo97@vp.pl>
2018-12-06 17:37:18 +0100

    webpack config

--- src/Coffeeland/Coffeeland/Client/webpack.config.js
+++ src/Coffeeland/Coffeeland/Client/webpack.config.js
@@ -2,12 +2,22 @@ const path = require('path');
 
 module.exports = {
     mode: "production",
-    entry: './App/App.js',
+    entry: './src/index.js',
     resolve: {
         extensions: [ '.js' ]
     },
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist')
-    }
+    },
+    module: {
+        rules: [
+          {
+            test: /\.css$/,
+            use: [
+              { loader: "css-loader" }
+            ]
+          }
+        ]
+      }
 };
