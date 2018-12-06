be20efec1bacb8d5c39bd24f8a74264f9f6f7568
MarekOchocki <marcopolo97@vp.pl>
2018-12-06 16:38:54 +0100

    clean up js

--- src/Coffeeland/Coffeeland/Client/App/App.js
+++ src/Coffeeland/Coffeeland/Client/App/App.js
@@ -2,36 +2,45 @@ import $ from 'jquery';
 window.jQuery = $;
 require('signalr');
 
-var init_foo = function() {
-// Declare a proxy to reference the hub.
-var chat = $.connection.communicationHub;
-// Create a function that the hub can call to broadcast messages.
-chat.client.broadcastMessage = function (name, message) {
-    // Html encode display name and message.
-    var encodedName = $('<div />').text(name).html();
-    var encodedMsg = $('<div />').text(message).html();
-    // Add the message to the page.
-    $('#discussion').append('<li><strong>' + encodedName
-        + '</strong>:&nbsp;&nbsp;' + encodedMsg + '</li>');
-};
-// Get the user name and store it to prepend to messages.
-$('#displayname').val(prompt('Enter your name:', ''));
-// Set initial focus to message input box.
-$('#message').focus();
-// Start the connection.
-console.log("start()");
-console.log($.connection.hub);
-$.connection.hub.start().done(function () {
-    console.log("start().done()");
-    console.log($('#sendmessage'));
-    $('#sendmessage').click(function () {
-        // Call the Send method on the hub.
-        console.log("wysylam");
-        chat.server.send($('#displayname').val(), $('#message').val());
-        // Clear text box and reset focus for next comment.
-        $('#message').val('').focus();
-    });
-});
-};
+var displayResponse = function(data) {
+    data = $('<div />').text(data).html();
+    $('#responses').append('<li><strong>' + data + '</li>');
+}
 
-$(init_foo);
+
+var set_buttons_for_tests = function(queryProcessor) {
+
+    $('#sendQuery1').click(function () {
+        queryProcessor.processQuery({name: "getSomeNumberQuery"}).then(data => displayResponse("1: " + data));;
+    });
+    $('#sendQuery2').click(function () {
+        queryProcessor.processQuery({name: "getStringQuery"}).then(data => displayResponse("2: " + data));;
+    });
+    $('#sendQuery3').click(function () {
+        queryProcessor.processQuery({name: "getSameResponseQuery", message: $('#message').val()}).then(data => displayResponse("3: " + data));;
+    });
+    $('#sendQuery4').click(function () {
+        queryProcessor.processQuery({name: "getSomeJsonQuery"}).then(data => displayResponse("4: " + data));
+    });
+}
+
+class QueryProcessor {
+    constructor() {
+        this.chat = $.connection.communicationHub;
+        $.connection.hub.start();
+        set_buttons_for_tests(this);
+    }
+
+    processQuery(query) {
+        return this.chat.server.send(JSON.stringify(query));
+    }
+}
+var queryProcessor;
+$(function() {
+    queryProcessor = new QueryProcessor();
+});
+
+
+
+
+
---
+++ src/Coffeeland/Coffeeland/GetSomeNumberQueryHandler.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Coffeeland
+{
+    public static class GetSomeNumberQueryHandler
+    {
+
+    }
+}
