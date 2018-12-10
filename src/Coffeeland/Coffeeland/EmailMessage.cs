d8868401c10773c4785f33cd976446e8b7e2ca70
Magda <magdtrag@gmail.com>
2018-12-10 22:31:19 +0100

    Add email sending mechanism

---
+++ src/Coffeeland/Coffeeland/EmailMessage.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MailKit.Net.Smtp;
+using MimeKit;
+
+
+namespace Coffeeland
+{
+    class EmailMessage
+    {
+        const String host = "smtp.gmail.com";
+        const int port = 465;
+        const bool useSsl = true;
+
+        const String fromName = "Coffeeland";
+        const String fromEmailAddress = "coffeeland.store@gmail.com";
+        const String password = ""; //fill
+
+        MimeMessage message;
+        BodyBuilder bodyBuilder;
+
+        public EmailMessage()
+        {
+            message = new MimeMessage();
+            message.From.Add(new MailboxAddress(fromName,fromEmailAddress));
+            bodyBuilder = new BodyBuilder();
+        }
+
+        public void AddReceiver(String toName, String toEmailAddress)
+        {
+            message.To.Add(new MailboxAddress(toName, toEmailAddress));
+        }
+
+        public void SetSubject(String subject)
+        {
+            message.Subject = subject;
+        }
+
+        public void SetEmailBody(String body)
+        {
+            bodyBuilder.TextBody = body;
+            message.Body = bodyBuilder.ToMessageBody();
+        }
+
+        public bool Send()
+        {
+            try
+            {
+                SmtpClient client = new SmtpClient();
+                client.Connect(host, port, useSsl);
+                client.Authenticate(fromEmailAddress, password);
+                client.Send(message);
+                client.Disconnect(true);
+                return true;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return false;
+            }
+        }
+
+        
+
+    }
+}
