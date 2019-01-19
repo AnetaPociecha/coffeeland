cbfb5c381a3b454acddeb7c42bc9ed103409e99f
anetapociecha <apociecha@interia.pl>
2019-01-19 23:32:46 +0100

    clean up

--- src/Coffeeland/Coffeeland/Client/src/components/shop/shopItemPage.jsx
+++ src/Coffeeland/Coffeeland/Client/src/components/shop/shopItemPage.jsx
@@ -22,10 +22,6 @@ class ShopItemPage extends React.Component {
     window.scrollTo(0, 0);
   }
 
-  componentWillMount() {
-    //this.props.fetchShopItems();
-  }
-
   state = {
     quantity: 1,
     shouldAddToCartMsgBeDisplayed: false
@@ -38,45 +34,52 @@ class ShopItemPage extends React.Component {
     } = this.props;
 
     const name = getItemName(params.name);
-    const item = items.items ? items.items.find(i => i.name === name) : {}
+    const item = items.items ? items.items.find(i => i.name === name) : {};
     const { quantity, shouldAddToCartMsgBeDisplayed } = this.state;
 
     return item ? (
-      <div className="row mx-auto border mb-5 mt-3" style={this.props.style}>
+      <div  className="row mx-auto" style={this.props.style}>
         {shouldAddToCartMsgBeDisplayed && (
           <div className="col-12 mt-4">
             <SuccessAlert>{message.PRODUCT_ADDED_TO_CART}</SuccessAlert>
           </div>
         )}
-
-        <div className="col-md-6 col-md-offset-5 pb-3 pl-3 pr-3 mt-3">
-          <Image src={item.img} />
-        </div>
-        <div className="col-md-6 col-md-offset-5 p-3 mt-3">
-          <OrderTitle>{item.name}</OrderTitle>
-          <Info>Type - {item.type}</Info>
-          <Info>Weight - {config.WEIGTH}kg</Info>
-          <Price className="pt-2" price={item.price} />
-          <div className="row">
-            <div display="block" className="p-3">
-              <InputSpinner
-                value={quantity}
-                onMinus={this.onQuantityDecrease}
-                onPlus={this.onQuantityIncrease}
-                min={config.MIN_QUANTITY}
-                max={config.MAX_QUANTITY}
-              />
-            </div>
-            <div display="block" className="p-3">
-              <Button onClick={() => this.onAddToCart(item)}>
-                {title.ADD_TO_CART}
-              </Button>
+        <div className="row border mb-5 mt-3" >
+          <div className="col-md-6 col-md-offset-5 pb-3 pl-3 pr-3 mt-3">
+            <Image src={item.img} />
+          </div>
+          <div className="col-md-6 col-md-offset-5 p-3 mt-3">
+            <OrderTitle>{item.name}</OrderTitle>
+            <Info>Type - {item.type}</Info>
+            <Info>Weight - {config.WEIGTH}kg</Info>
+            <Price className="pt-2" price={item.price} />
+            <div className="row">
+              <div display="block" className="p-3">
+                <InputSpinner
+                  value={quantity}
+                  onMinus={this.onQuantityDecrease}
+                  onPlus={this.onQuantityIncrease}
+                  min={config.MIN_QUANTITY}
+                  max={config.MAX_QUANTITY}
+                />
+              </div>
+              <div display="block" className="p-3">
+                <Button onClick={() => this.onAddToCart(item)}>
+                  {title.ADD_TO_CART}
+                </Button>
+              </div>
             </div>
           </div>
+          <div>
+            <Info className="pr-3 pl-3 pb-4 text-justify">
+              {item.description}
+            </Info>{" "}
+          </div>
         </div>
-        <Info className="p-2 text-justify"> {item.description}</Info>
       </div>
-    ) : <Redirect to={SHOP} />;
+    ) : (
+      <Redirect to={SHOP} />
+    );
   }
 
   onAddToCart = item => {
